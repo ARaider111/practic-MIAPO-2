e76c77d8f09b7c51c19e39302db914410f992411
ARaider111 <pisarovalexander@yandex.ru>
2025-06-14 10:50:42 +0700

    add function subtraction

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+
+def Addition():
+    a = float(input("Введите первое число: "))
+    b = float(input("Введите второе число: "))
+    print(f"Результат: {a} + {b} = {a + b}")
+
+def Subtraction():
+    a = float(input("Введите первое число: "))
+    b = float(input("Введите второе число: "))
+    print(f"Результат: {a} - {b} = {a - b}")
+
 def PrintMenu():
     print("\n--- Главное меню ---")
+    print("1. Сложение")
+    print("2. Вычитание")
+    print("3. Умножение")
+    print("4. Деление")
     print("0. Выход")
 
 def main():
@@ -7,7 +22,11 @@ def main():
         PrintMenu()
         num = input("Выберите пункт меню: ")
 
-        if num == '0':
+        if num == '1':
+            Addition()
+        elif num == '2':
+            Subtraction()
+        elif num == '0':
             print("Выход из программы.")
             break
         else:
