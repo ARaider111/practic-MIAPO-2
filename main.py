669a95245cae8d2b0ff3803096a482dad8bc668b
ARaider111 <pisarovalexander@yandex.ru>
2025-06-14 10:57:40 +0700

    add function division

--- main.py
+++ main.py
@@ -1,13 +1,49 @@
+
+def Addition():
+    a = float(input("Введите первое число: "))
+    b = float(input("Введите второе число: "))
+    print(f"Результат: {a} + {b} = {a + b}")
+
+def Subtraction():
+    a = float(input("Введите первое число: "))
+    b = float(input("Введите второе число: "))
+    print(f"Результат: {a} - {b} = {a - b}")
+
+def Multiplication():
+    a = float(input("Введите первое число: "))
+    b = float(input("Введите второе число: "))
+    print(f"Результат: {a} * {b} = {a * b}")
+
+def Division():
+    a = float(input("Введите первое число: "))
+    b = float(input("Введите второе число: "))
+    if b == 0:
+        print("Ошибка: деление на ноль невозможно.")
+    else:
+        print(f"Частное: {a} / {b} = {a / b}")
+
 def PrintMenu():
-    print("\n--- Главное меню ---")
+    print("\n--=- Главное меню --=-")
+    print("1. Сложение")
+    print("2. Вычитание")
+    print("3. Умножение")
+    print("4. Деление")
     print("0. Выход")
 
 def main():
     while True:
         PrintMenu()
-        num = input("Выберите пункт меню: ")
+        number = input("Выберите пункт меню: ")
 
-        if num == '0':
+        if number == '1':
+            Addition()
+        elif number == '2':
+            Subtraction()
+        elif number == '3':
+            Multiplication()
+        elif number == '3':
+            Division()
+        elif number == '0':
             print("Выход из программы.")
             break
         else:
